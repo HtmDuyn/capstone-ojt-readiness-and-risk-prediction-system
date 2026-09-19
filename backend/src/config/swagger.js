4137a08e7692bbf372b958d9aba2c1f2b0d1f444
HtmDuyn <hg2922004@gmail.com>
2026-09-19 14:21:54 +0700

    update lại file swager

--- backend/src/config/swagger.js
+++ backend/src/config/swagger.js
@@ -10,6 +10,10 @@ const options = {
         },
         servers: [
             {
+                url: "https://capstone-ojt-readiness-and-risk.onrender.com",
+                description: "Production Server"
+            },
+            {
                 url: "http://localhost:3000",
                 description: "Local Development"
             }
